3467f77a4a29d0a672d0ab5ea333d1daa84ae654
SalmaElhila <elhilasalma@gmail.com>
2023-05-30 02:58:07 -0700

    new mod

--- print_functions.c
+++ print_functions.c
@@ -11,8 +11,8 @@ int print_char(va_list ap, params_t *params)
 {
 	char pad_char = ' ';
 	unsigned int pad = 1, sum = 0, ch = va_arg(ap, int);
-	
 	if (params->minus_flag)
+
 		sum += _putchar(ch);
 	while (pad++ < params->width)
 		sum += _putchar(pad_char);
@@ -32,7 +32,6 @@ int print_char(va_list ap, params_t *params)
 int print_int(va_list ap, params_t *params)
 {
 	long l;
-	
 	if (params->l_modifier)
 		l = va_arg(ap, long);
 	else if (params->h_modifier)
@@ -46,7 +45,7 @@ int print_int(va_list ap, params_t *params)
  * print_string - prints a string
  * @ap: The argument pointer
  * @params: The parameter structure
- *     
+ *
  * Return: number char printed
  **/
 
@@ -63,7 +62,7 @@ int print_string(va_list ap, params_t *params)
 			j = pad = params->precision;
 		if (params->minus_flag)
 		{
-			if (params->precision !=UNIT_MAX)
+			if (params->precision != UNIT_MAX)
 				for (i = 0; i < pad; i++)
 					sum += _putchar(*str++);
 			elsesum += _puts(str);
@@ -108,15 +107,14 @@ int print_S(va_list ap, params_t *params)
 	char *str = va_arg(ap, char *);
 	char *hex;
 	int sum = 0;
-	
 	if ((int)(!str))
 		return (_puts(NULL_STRING));
-	for (;  *str;str++)
+	for (; *str; str++)
 		if ((*str > 0 && *str < 32) || *str >= 127)
 		{
 			sum += _putchar('\\') ;
-			sum += _putchar('x') ;
-			hex = convert(*str, 16,0, params);
+			sum += _putchar('x');
+			hex = convert(*str, 16, 0, params);
 			if (!hex[1])
 				sum += _putchar('0');
 			sum += _puts(hex);
